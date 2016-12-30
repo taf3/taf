538666e5b2c826a17c7b9c7d77adc53057512f80
Tkachuk <nazarx.tkachuk@intel.com>
2016-12-30 04:45:37 -0800

    Fixed copyright in dpdk.py file according to Apache v2.0
    Change-Id: If6f89cf45096656ea2bce073405052bd691044e8
    Signed-off-by: Tkachuk <nazarx.tkachuk@intel.com>

--- taf/testlib/linux/dpdk.py
+++ taf/testlib/linux/dpdk.py
@@ -1,14 +1,17 @@
 """
 @copyright Copyright (c) 2016, Intel Corporation.
 
-This program is free software; you can redistribute it and/or modify it
-under the terms and conditions of the GNU Lesser General Public License,
-version 2.1, as published by the Free Software Foundation.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-This program is distributed in the hope it will be useful, but WITHOUT ANY
-WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for
-more details.
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 
 @file  dpdk.py
 
