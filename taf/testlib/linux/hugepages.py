2cd461b1c3498350155bedad081f9e207d014399
Savchuk, RomanX <romanx.savchuk@intel.com>
2017-01-04 11:50:29 +0200

    Done minor changes in hugepages methods for ability to test 2kB or 1G hugepages
    Change-Id: If34fe085d069573dab1c8c4edb5d16bfcbe6092d
    Signed-off-by: Savchuk, RomanX <romanx.savchuk@intel.com>

--- taf/testlib/linux/hugepages.py
+++ taf/testlib/linux/hugepages.py
@@ -17,6 +17,7 @@ limitations under the License.
 
 @summary Class to abstract hugepages operations
 """
+DEFAULT_HUGEPAGE_SIZE = 2048
 
 
 class HugePages(object):
@@ -28,38 +29,37 @@ class HugePages(object):
         super(HugePages, self).__init__()
         self.cli_send_command = cli_send_command
 
-    def mount(self, nr_hugepages, hugepage_size='2048kB'):
+    def mount(self, nr_hugepages, hugepage_size=DEFAULT_HUGEPAGE_SIZE):
         """
         @brief  Mount hugepages
         @param nr_hugepages: Number of hugepages
         @type nr_hugepages: int
         @param hugepage_size: Current hugepage allocated size
-        @type hugepage_size: str
+        @type hugepage_size: int
         """
         self.cli_send_command("mkdir -p /mnt/huge")
         self.change_number(nr_hugepages, hugepage_size)
         self.cli_send_command("mount -t hugetlbfs nodev /mnt/huge")
 
-    def get_number(self, hugepage_size='2048kB'):
+    def get_number(self, hugepage_size=DEFAULT_HUGEPAGE_SIZE):
         """
         @brief  Get number of hugepages
         @param hugepage_size: Current hugepage allocated size
-        @type hugepage_size: str
+        @type hugepage_size: int
         @rtype:  int
         @return:  Returns number of hugepages
         """
-        output = self.cli_send_command(
-            command='cat /sys/devices/system/node/node0/hugepages/hugepages-{}/nr_hugepages'.format(hugepage_size)).stdout
+        command_template = 'cat /sys/devices/system/node/node0/hugepages/hugepages-{}kB/nr_hugepages'
+        output = self.cli_send_command(command=command_template.format(hugepage_size)).stdout
         return int(output)
 
-    def change_number(self, nr_hugepages, hugepage_size='2048kB'):
+    def change_number(self, nr_hugepages, hugepage_size=DEFAULT_HUGEPAGE_SIZE):
         """
         @brief  Change number of hugepages
         @param nr_hugepages: Number of hugepages
         @type nr_hugepages: int
         @param hugepage_size: Current hugepage allocated size
-        @type hugepage_size: str
+        @type hugepage_size: int
         """
-        self.cli_send_command(
-            command='echo {} > /sys/devices/system/node/node0/hugepages/hugepages-{}/nr_hugepages'.format(nr_hugepages,
-                                                                                                          hugepage_size))
+        command_template = 'echo {0} > /sys/devices/system/node/node0/hugepages/hugepages-{1}kB/nr_hugepages'
+        self.cli_send_command(command=command_template.format(nr_hugepages, hugepage_size))
