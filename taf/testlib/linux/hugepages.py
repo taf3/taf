2857dd1b1eff26b5f36ebd46363423178c13842a
Orest Kreminskyi <orestx.kreminskyi@intel.com>
2017-03-06 15:38:53 +0200

    Extended hugepages module:
     - added different HugePage sizes
     - added per node hugepage configuration
     - added umount method
    
    Signed-off-by: Orest Kreminskyi <orestx.kreminskyi@intel.com>

--- taf/testlib/linux/hugepages.py
+++ taf/testlib/linux/hugepages.py
@@ -1,5 +1,5 @@
 """
-@copyright Copyright (c) 2016, Intel Corporation.
+@copyright Copyright (c) 2016-2017, Intel Corporation.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -29,37 +29,95 @@ class HugePages(object):
         super(HugePages, self).__init__()
         self.cli_send_command = cli_send_command
 
-    def mount(self, nr_hugepages, hugepage_size=DEFAULT_HUGEPAGE_SIZE):
+    def _hugepages_helper(self, kwargs):
+        """
+        @brief  Helper function for getting and setting hugepages
+        @param kwargs: Arguments to pass
+        @type kwargs: dict
+        @rtype tuple(str, str, int) | CmdStatus
+        @return  Returns CmdStatus namedtuple of stdout, stderr, return code
+        """
+        if kwargs.get('per_node') is not None:
+            template = '{mode} /sys/devices/system/node/node{per_node}/hugepages/hugepages-{hugepage_size}kB/{param_name}'
+        else:
+            template = '{mode} /sys/kernel/mm/hugepages/hugepages-{hugepage_size}kB/{param_name}'
+
+        command = template.format(**kwargs)
+        return self.cli_send_command(command)
+
+    def mount(self, nr_hugepages, per_node=None, mnt_dir='/mnt/huge', hugepage_size=DEFAULT_HUGEPAGE_SIZE, **kwargs):
         """
         @brief  Mount hugepages
         @param nr_hugepages: Number of hugepages
         @type nr_hugepages: int
+        @param per_node: Mount per node or per system
+        @type per_node: int
+        @param mnt_dir: Mount dir
+        @type mnt_dir: str
         @param hugepage_size: Current hugepage allocated size
         @type hugepage_size: int
+        @param kwargs: Additional arguments to pass to commandline
+        @type kwargs: dict
         """
-        self.cli_send_command("mkdir -p /mnt/huge")
-        self.change_number(nr_hugepages, hugepage_size)
-        self.cli_send_command("mount -t hugetlbfs nodev /mnt/huge")
+        self.cli_send_command("mkdir -p {}".format(mnt_dir))
+        self.change_number(nr_hugepages, per_node=per_node, hugepage_size=hugepage_size)
+        cmd = ['mount', '-t', 'hugetlbfs', 'nodev', mnt_dir]
+        if kwargs:
+            cmd.append(' '.join('--{} {}'.format(x, y) for x, y in kwargs.items()))
+        cmd = ' '.join(cmd)
+        self.cli_send_command(cmd)
 
-    def get_number(self, hugepage_size=DEFAULT_HUGEPAGE_SIZE):
+    def umount(self, mnt_dir='/mnt/huge', **kwargs):
+        """
+        @brief  Unmount hugepages
+        @param mnt_dir: Dir to unmount
+        @type mnt_dir: str
+        @param kwargs: Additional arguments to pass to commandline
+        @type kwargs: dict
+        """
+        cmd = ['umount', mnt_dir]
+        if kwargs:
+            cmd.append(' '.join('--{} {}'.format(x, y) for x, y in kwargs.items()))
+        cmd = ' '.join(cmd)
+        self.cli_send_command(cmd)
+
+    def get_number(self, per_node=None, hugepage_size=DEFAULT_HUGEPAGE_SIZE):
         """
         @brief  Get number of hugepages
+        @param per_node: Get number of hugepages per node or per system
+        @type per_node: int
         @param hugepage_size: Current hugepage allocated size
         @type hugepage_size: int
         @rtype:  int
         @return:  Returns number of hugepages
         """
-        command_template = 'cat /sys/devices/system/node/node0/hugepages/hugepages-{}kB/nr_hugepages'
-        output = self.cli_send_command(command=command_template.format(hugepage_size)).stdout
+        output = self._hugepages_helper({'mode': 'cat', 'per_node': per_node, 'hugepage_size': hugepage_size,
+                                         'param_name': 'nr_hugepages'}).stdout
         return int(output)
 
-    def change_number(self, nr_hugepages, hugepage_size=DEFAULT_HUGEPAGE_SIZE):
+    def get_free_memory(self, per_node=None, hugepage_size=DEFAULT_HUGEPAGE_SIZE):
+        """
+        @brief  Get free hugepages
+        @param per_node: Get number of free hugepages per node or per system
+        @type per_node: int
+        @param hugepage_size: Current hugepage allocated size
+        @type hugepage_size: int
+        @rtype:  int
+        @return:  Returns number of free hugepages
+        """
+        output = self._hugepages_helper({'mode': 'cat', 'per_node': per_node, 'hugepage_size': hugepage_size,
+                                         'param_name': 'free_hugepages'}).stdout
+        return int(output)
+
+    def change_number(self, nr_hugepages, per_node=None, hugepage_size=DEFAULT_HUGEPAGE_SIZE):
         """
         @brief  Change number of hugepages
         @param nr_hugepages: Number of hugepages
         @type nr_hugepages: int
+        @param per_node: Number of hugepages per node or per system
+        @type per_node: int
         @param hugepage_size: Current hugepage allocated size
         @type hugepage_size: int
         """
-        command_template = 'echo {0} > /sys/devices/system/node/node0/hugepages/hugepages-{1}kB/nr_hugepages'
-        self.cli_send_command(command=command_template.format(nr_hugepages, hugepage_size))
+        self._hugepages_helper({'mode': 'echo {} >'.format(nr_hugepages), 'per_node': per_node,
+                                'hugepage_size': hugepage_size, 'param_name': 'nr_hugepages'})
